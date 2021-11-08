08bf68e9e484f047e4e3f6259cf959d4e271d176
illus0r <zor667@gmail.com>
2021-11-08 17:31:10 +0300

    transmission

---
+++ transmission_glitch/draw.frag
@@ -0,0 +1,72 @@
+#version 300 es
+precision mediump float;
+uniform vec2 u_resolution;
+uniform vec2 u_tex_res;
+uniform sampler2D tex;
+uniform sampler2D backbuffer;
+// uniform float midi[64];
+uniform float u_time;
+uniform vec4 palette[5];
+out vec4 o;
+
+#define PI 3.1415
+
+// #define col(c) -cos((pow(vec3(c), pw) + off) * 2. * PI) * mul + add
+// vec3 pw = vec3(midi[0 + 16], midi[1 + 16], midi[2 + 16]);
+// vec3 off = vec3(midi[4 + 16], midi[5 + 16], midi[6 + 16]);
+// vec3 mul = vec3(midi[8 + 16], midi[9 + 16], midi[10 + 16]);
+// vec3 add = vec3(midi[12 + 16], midi[13 + 16], midi[14 + 16]);
+
+// float size = midi[15 + 16 * 2] * 10000.;
+
+// vec3 col(float c) {
+//   vec4 c1, c2;
+//   c1 = palette[0];
+//   c2 = palette[2];
+//   if(c > .5) {
+//     c1 = palette[2];
+//     c2 = palette[3];
+//   }
+//   return mix(c1, c2, fract(c * 1.999)).rgb;
+// }
+
+#define rnd(d) fract(sin(length(d)*99.))
+#define rot(a) mat2(cos(a),-sin(a),sin(a),cos(a))
+
+void main() {
+  vec2 uvN = gl_FragCoord.xy / u_resolution;
+  vec2 uv = (gl_FragCoord.xy * 2. - u_resolution) / min(u_resolution.x, u_resolution.y);
+
+  // float sea = texture(prevStateSea, uvN).r;
+
+  // float steps = 100. * midi[16 + 7];
+  // sea = floor(sea * steps) / steps;
+
+  // vec4 seaCol = vec4(col(sea), 1);
+
+  // // // vec2 U = (gl_FragCoord.xy * 2. - u_resolution) / min(u_resolution.x, u_resolution.y);
+
+  // vec2 U = uv;
+  // U += 1.;
+  // U /= 2.;
+  // // U = U * 8.;
+  // // U -= 1. / 1.6 / 2.;
+  // vec2 uvFl = floor(U * 17.) / 17.;
+  // vec2 uvFr = fract(U * 17.) * 2. - 1.;
+  // float conway = texture(prevStateConway, uvFl).r * smoothstep(3., 0., length(uvFr * uvFr * uvFr * uvFr * uvFr * uvFr));
+
+  // // if(conway > sea)
+  // //   gl_FragColor = mix(1.-seaCol, seaCol, smoothstep(.1, .0, conway-sea));
+  // // else{
+  // //   gl_FragColor = seaCol;
+  // // }
+  // // gl_FragColor = texture(prevStateConway, U);
+  // // gl_FragColor.rg = uv;
+  // gl_FragColor = mix(vec4(rnd(sea)) * vec4(midi[16 * 2 + 0], midi[16 * 2 + 1], midi[16 * 2 + 2], 1), seaCol, smoothstep(.1, .0, conway - sea));
+
+  // // gl_FragColor = mix(gl_FragColor, texture(backbuffer, uv), 0.);
+  // // gl_FragColor.rgb = col(c);
+
+  o = texture(tex, uvN);
+  o.a = 1.;
+}
---
+++ transmission_glitch/index.js
@@ -0,0 +1,292 @@
+'use strict';
+let twgl = require('twgl.js')
+let chroma = require('chroma-js')
+const vShader = `#version 300 es
+  precision mediump float;
+  in vec2 position;
+  void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+  }`;
+const fShader = require('./shader.frag');
+let timePrev = +new Date()
+let time = 0
+
+const mousepos = [0, 0];
+let pause = false
+let tick = 0
+const canvas = document.getElementById('canvasgl')
+const gl = canvas.getContext("webgl2", { preserveDrawingBuffer: true })
+let passes
+
+function Pass({ frag, size = 8, texture }) {
+  if (size.length)
+    this.resolution = size
+  else
+    this.resolution = [size, size]
+
+  console.log(this.resolution)
+  this.vert = `#version 300 es
+  precision mediump float;
+  in vec2 position;
+  void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+  }`
+  this.frag = frag
+  this.program = twgl.createProgramInfo(gl, [this.vert, this.frag])
+  // this.attachments = [{ format: gl.RGBA, type: gl.UNSIGNED_BYTE, minMag: gl.GL_NEAREST, wrap: gl.GL_CLAMP_TO_EDGE }]
+  this.attachments = [{ format: gl.RGBA, mag: gl.NEAREST }]
+  this.buffer = twgl.createFramebufferInfo(gl, this.attachments, ...this.resolution)
+  this.backbuffer = twgl.createFramebufferInfo(gl, this.attachments, ...this.resolution)
+  this.b = this.backbuffer.attachments[0]
+  this.positionObject = { position: { data: [1, 1, 1, -1, -1, -1, -1, 1], numComponents: 2 } }
+  this.positionBuffer = twgl.createBufferInfoFromArrays(gl, this.positionObject)
+
+  this.texture = texture
+
+  this.draw = ({ uniforms, target }) => {
+    // target: self, screen, self+screen
+    gl.useProgram(this.program.program)
+    twgl.setBuffersAndAttributes(gl, this.program, this.positionBuffer)
+
+    if (!uniforms.u_resolution) uniforms.u_resolution = this.resolution
+    if (target != 'screen') // self or both
+      uniforms.backbuffer = this.backbuffer.attachments[0]
+    if (this.texture)
+      uniforms.texture = this.texture
+    twgl.setUniforms(this.program, uniforms)
+
+    if (target != 'self') { // screen or both
+      twgl.bindFramebufferInfo(gl, null)
+      twgl.drawBufferInfo(gl, this.positionBuffer, gl.TRIANGLE_FAN)
+    }
+    if (target != 'screen') { // self or both
+      twgl.bindFramebufferInfo(gl, this.buffer)
+      let tmp = this.buffer
+      this.buffer = this.backbuffer
+      this.backbuffer = tmp
+      this.b = this.backbuffer.attachments[0]
+      twgl.drawBufferInfo(gl, this.positionBuffer, gl.TRIANGLE_FAN)
+    }
+  }
+}
+
+
+import palettes from "z-palletes"
+console.log(palettes)
+let palette
+let params
+
+let isCanvasSquare = false
+////////////////////
+// CUSTOM CODE BELOW
+////////////////////
+let dartTheme = true
+
+
+////////////////////
+// CUSTOM CODE END
+////////////////////
+
+twgl.addExtensionsToContext(gl);
+gl.getExtension("OES_texture_float")
+gl.getExtension("WEBGL_color_buffer_float")
+
+
+
+
+
+
+
+mouseClicked()
+windowResized()
+
+
+
+
+
+
+passes = {
+  rnd: new Pass({
+    frag: require('./rnd.frag'),
+    size: 8,
+  }),
+  ca: new Pass({
+    frag: require('./ca.frag'),
+    size: 128,
+  }),
+  draw: new Pass({
+    frag: require('./draw.frag'),
+    size: 1024,
+  }),
+}
+
+function draw() {
+  if(!passes || !passes.draw || !passes.draw.program) return;
+  passes.rnd.draw({ uniforms: { tick: tick, }, target: 'self', })
+  passes.ca.draw({ uniforms: { tick: tick, tex: passes.rnd.b, divisions: 1, }, target: 'self', })
+  passes.ca.draw({ uniforms: { tick: tick, tex: passes.ca.b, divisions: 2, }, target: 'self', })
+  passes.ca.draw({ uniforms: { tick: tick, tex: passes.ca.b, divisions: 3, }, target: 'self', })
+  passes.ca.draw({ uniforms: { tick: tick, tex: passes.ca.b, divisions: 4, }, target: 'self', })
+  passes.ca.draw({ uniforms: { tick: tick, tex: passes.ca.b, divisions: 5, }, target: 'self', })
+  passes.draw.draw({
+    uniforms: {
+      tex: passes.ca.b,
+      u_time: time / 1000,
+      palette: palette.flat(),
+      u_resolution: [canvas.width, canvas.height], // window.devicePixelRatio
+    },
+    target: 'screen',
+  })
+
+  tick++
+}
+
+function animate() {
+  if (pause) return;
+  let timeCurrent = +new Date()
+  time += timeCurrent - timePrev
+  timePrev = timeCurrent
+  draw()
+  requestAnimationFrame(animate)
+}
+animate()
+
+function setMousePos(e) {
+  mousepos[0] = e.clientX / gl.canvas.clientWidth;
+  mousepos[1] = 1 - e.clientY / gl.canvas.clientHeight;
+}
+
+canvas.addEventListener('mousemove', setMousePos);
+
+// canvas.addEventListener('mouseleave', () => {
+//   mousepos[0] = .501;
+//   mousepos[1] = .501;
+// });
+
+function handleTouch(e) {
+  e.preventDefault();
+  setMousePos(e.touches[0]);
+}
+
+
+
+document.querySelector('canvas').addEventListener('click', mouseClicked)
+window.addEventListener('touchstart', mouseClicked)
+function mouseClicked() {
+  let paletteId = Math.floor(palettes.length * Math.random())
+  console.log(paletteId)
+  console.log(palettes[paletteId])
+  palette = palettes[paletteId].map(c => chroma(c).gl())
+  // palette = palette.sort((a, b) => chroma(a).get('lch.l') - chroma(b).get('lch.l'))
+  params = [Math.random(), Math.random(), Math.random(), Math.random(),]
+
+  ////////////////////
+  // CUSTOM CODE BELOW
+  ////////////////////
+  dartTheme = !dartTheme
+
+
+
+  ////////////////////
+  // CUSTOM CODE END
+  ////////////////////
+  draw()
+}
+
+window.addEventListener('resize', windowResized)
+function windowResized() {
+  if (isCanvasSquare) {
+    let winMinSize = Math.min(window.innerWidth, window.innerHeight)
+    canvas.width = canvas.height = winMinSize * window.devicePixelRatio
+    document.querySelector('canvas').style.width = document.querySelector('canvas').style.heigth = winMinSize + 'px'
+  }
+  else {
+    canvas.width = window.innerWidth * window.devicePixelRatio
+    canvas.height = window.innerHeight * window.devicePixelRatio
+    document.querySelector('canvas').style.width = window.innerWidth + 'px'
+    document.querySelector('canvas').style.heigth = window.innerHeight + 'px'
+  }
+}
+
+let blink = () => {
+  document.querySelector('canvas').style.opacity = 0
+  setTimeout(() => document.querySelector('canvas').style.opacity = 1, 100)
+}
+
+let timer
+window.addEventListener('keypress', keyPressed)
+function keyPressed(key) {
+  if (key.code.slice(0, 5) == 'Digit') {
+    let digit = Number(key.key)
+    if (digit == 0) {
+      blink()
+      clearInterval(timer)
+    }
+    else {
+      if (!digit) return // NaN check, just in case
+      clearInterval(timer)
+      blink()
+      mouseClicked()
+      timer = setInterval(() => { mouseClicked() }, 1.5 ** digit * 400)
+    }
+  }
+  if (key.code == 'Space') {
+    pause = !pause
+    if (pause) {
+    }
+    else {
+      timePrev = +new Date()
+      animate()
+    }
+  }
+  if (key.code == 'KeyS') {
+    saveImage()
+  }
+}
+
+function saveImage() {
+  let size = 10000
+  let splits = Math.ceil(size / 512)
+  let step = 1 / splits
+  let splitSize = size / splits
+  canvas.width = splitSize
+  canvas.height = splitSize
+
+  var dynamicCanvas = document.createElement("canvas");
+  var dynamicContext = dynamicCanvas.getContext("2d")
+  dynamicCanvas.height = size;
+  dynamicCanvas.width = size;
+
+  let date = new Date()
+  for (let i = 0; i < 1; i += step) {
+    for (let j = 0; j < 1; j += step) {
+      gl.useProgram(program.program);
+      twgl.setBuffersAndAttributes(gl, program, positionBuffer);
+      twgl.setUniforms(program, {
+        // prevStateCells: shader.attachments[0],
+        tick: tick,
+        palette: palette.flat(),
+        u_time: time / 1000,
+        u_resolution: [gl.canvas.width, gl.canvas.height],
+        u_mouse: mousepos,
+        params: params,
+        viewbox: [i, j, step, step],
+        dartTheme: dartTheme,
+      });
+      twgl.bindFramebufferInfo(gl, null);
+      twgl.drawBufferInfo(gl, positionBuffer, gl.TRIANGLE_FAN);
+
+      dynamicContext.drawImage(canvas, i * size, size - (j + step) * size);
+    }
+  }
+
+
+  let link = document.getElementById('link');
+  link.setAttribute('download', `image.png`);
+  link.setAttribute('href', dynamicCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+  link.click();
+
+  // resume
+  windowResized()
+  draw()
+}
