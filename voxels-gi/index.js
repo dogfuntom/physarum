181c72463a020efc10194c50ce7844751f5e5e59
Maxim Kamalov <MaximKamalov@outlook.com>
2022-02-01 00:33:50 +0300

    added basic dependency injection, but...
    but moved z-shader-wrapper here, for easier development (at this stage),
    
    also, removed unused code, added JSDoc, unified code style,
    
    also, removed unused dependencies,
    
    also, fixed non-critical warnings, added minimal dev-ops, added a readme

--- voxels-gi/index.js
+++ voxels-gi/index.js
@@ -1,57 +1,45 @@
-'use strict';
+// @ts-check
 
-let twgl = require('twgl.js')
-let chroma = require('chroma-js');
-const { random } = require('chroma-js');
-
-import {printMsg} from "z-shader-wrapper"
-printMsg()
-
-// Palette
-let palette = ['#230f2b', '#f21d41', '#ebebbc', '#bce3c5', '#82b3ae']
-palette = palette.map(c => chroma(c).gl())
-palette = palette.sort((a, b) => chroma(a).get('lch.l') - chroma(b).get('lch.l'))
+import twgl from 'twgl.js'
+import { ZShaderWrapper } from './z-shader-wrapper-2'
 
 // WebGL
-const canvas = document.getElementById('canvasgl')
-const gl = canvas.getContext("webgl2", { preserveDrawingBuffer: true })
-gl.getExtension('EXT_color_buffer_float'); // prevents Buffer Incomplete error
-gl.getExtension('OES_texture_float_linear');
-
-let passes
+const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('canvasgl'))
+const gl = canvas.getContext('webgl2', { preserveDrawingBuffer: true })
+gl.getExtension('EXT_color_buffer_float') // prevents Buffer Incomplete error
+gl.getExtension('OES_texture_float_linear')
 
 // Scene
-let voxelsNum = 128
+const voxelsNum = 128
 let segments, relief
 
+function prepare2dSegmentsMap () {
+  relief = [...Array(voxelsNum)].map((d) => [...Array(voxelsNum)].map((d) => Math.random()))
+  segments = [...Array(voxelsNum)].map((d) => [...Array(voxelsNum)].fill(0))
 
-function prepare2dSegmentsMap() {
-  relief = [...Array(voxelsNum)].map((d) => [...Array(voxelsNum)].map((d) => Math.random()));
-  segments = [...Array(voxelsNum)].map((d) => [...Array(voxelsNum)].fill(0));
-
-  function getRelief(i, j) {
-    i = (i + voxelsNum) % voxelsNum;
-    j = (j + voxelsNum) % voxelsNum;
-    return relief[i][j];
+  function getRelief (i, j) {
+    i = (i + voxelsNum) % voxelsNum
+    j = (j + voxelsNum) % voxelsNum
+    return relief[i][j]
   }
 
   for (let x = 0; x < voxelsNum; x++) {
     for (let y = 0; y < voxelsNum; y++) {
-      let S = 3;
-      let minVal = Infinity;
-      let idx = x;
-      let idy = y;
+      const S = 3
+      let minVal = Infinity
+      let idx = x
+      let idy = y
       for (let step = 0; step < 3; step++) {
         let imin = 0
         let jmin = 0
         for (let i = -S; i <= S; i++) {
           for (let j = -S; j <= S; j++) {
-            if (Math.hypot(i, j) > S) continue;
-            let r = getRelief(idx + i, idy + j);
+            if (Math.hypot(i, j) > S) continue
+            const r = getRelief(idx + i, idy + j)
             if (r < minVal) {
-              minVal = r;
-              imin = i;
-              jmin = j;
+              minVal = r
+              imin = i
+              jmin = j
             }
           }
         }
@@ -59,109 +47,87 @@ function prepare2dSegmentsMap() {
         idy += jmin
       }
 
-      segments[x][y] = minVal * 1e3 - Math.floor(minVal * 1e3);
+      segments[x][y] = minVal * 1e3 - Math.floor(minVal * 1e3)
     }
   }
-  console.log("segments, relief", segments, relief);
+  console.log('segments, relief', segments, relief)
 }
 
-let rnd = (x) => {
-  let s = x * 9e4
+const rnd = (x) => {
+  const s = x * 9e4
   return s - Math.floor(s)
 }
-function prepareTexVoxels() {
+function prepareTexVoxels () {
   // fill 3d array N×N×N filled with rgba(0,0,0,0)
-  let texVoxelsArray = [...Array(voxelsNum)].map(() => [...Array(voxelsNum)].map(() => [...Array(voxelsNum)].map(_ => [0, 0, 0, 0])))
-  
+  const texVoxelsArray = [...Array(voxelsNum)].map(() => [...Array(voxelsNum)].map(() => [...Array(voxelsNum)].map(_ => [0, 0, 0, 0])))
+
   prepare2dSegmentsMap()
 
   for (let x = 1; x < voxelsNum; x++) {
     for (let z = 1; z < voxelsNum; z++) {
-      let id = segments[x][z]
-      let height = -63;
-      if (id == segments[x - 1][z] && id == segments[x][z - 1] && id == segments[x - 1][z - 1])
-        height = 4 + 8 * rnd(id);
+      const id = segments[x][z]
+      let height = -63
+      if (id === segments[x - 1][z] && id === segments[x][z - 1] && id === segments[x - 1][z - 1]) {
+        height = 4 + 8 * rnd(id)
+      }
       for (let y = 0; y < 64 + height; y++) {
-        texVoxelsArray[x][y][z] = [id, 0, 0, 0].map(d=>d*255)
+        texVoxelsArray[x][y][z] = [id, 0, 0, 0].map(d => d * 255)
       }
     }
   }
 
-  // texVoxelsArray[zz][yy][xx] = [xx*2,yy*2,zz*2,1]
   console.log('texVoxelsArray', texVoxelsArray)
 
-  let texVoxels = twgl.createTexture(gl, {
+  const texVoxels = twgl.createTexture(gl, {
     src: texVoxelsArray.flat(3),
     width: texVoxelsArray[0].length,
     mag: gl.NEAREST,
-    min: gl.NEAREST,
-  });
+    min: gl.NEAREST
+  })
 
   return texVoxels
 }
 
-let texVoxels = prepareTexVoxels()
-
-
-import {Pass} from "z-shader-wrapper"
+const texVoxels = prepareTexVoxels()
 
 let tick = 0
 
-let dt;
-let prevTime;
+twgl.resizeCanvasToDisplaySize(gl.canvas)
+const zsw = new ZShaderWrapper(gl, twgl)
 
-
-
-twgl.resizeCanvasToDisplaySize(gl.canvas);
-passes = {
-  gi: new Pass({
-    gl,
-    twgl,
-    frag: require('./gi.frag'),
-    size: [canvas.width, canvas.height],
-  }),
-  draw: new Pass({
-    gl,
-    twgl,
-    frag: require('./draw.frag'),
-  }),
+const passes = {
+  gi: zsw.createPass(require('./gi.frag'), [canvas.width, canvas.height]),
+  draw: zsw.createPass(require('./draw.frag'))
 }
 
-let params = [...Array(10)].map(() => Math.random())
+const params = [...Array(10)].map(() => Math.random())
 
-let timeI = new Date() / 1000
+const timeI = new Date().getSeconds()
 
-function draw() {
-  let time = new Date() / 1000
-  twgl.resizeCanvasToDisplaySize(gl.canvas);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  dt = (prevTime) ? time - prevTime : 0;
-  prevTime = time;
+function draw () {
+  const time = new Date().getSeconds()
+  twgl.resizeCanvasToDisplaySize(gl.canvas)
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
   passes.gi.draw({
-    uniforms: {
-      u_frame: tick,
-      tex: passes.gi.b,
-      u_time: time - timeI,
-      u_params: params,
-      u_tex_voxels: texVoxels,
-      u_voxels_num: voxelsNum,
-    },
-    target: 'self',
-  })
-  // console.log(time - timeI)
+    u_frame: tick,
+    tex: passes.gi.b,
+    u_time: time - timeI,
+    u_params: params,
+    u_tex_voxels: texVoxels,
+    u_voxels_num: voxelsNum
+  }, 'self')
+
   passes.draw.draw({
-    uniforms: {
-      tex: passes.gi.b,
-      u_resolution: [canvas.width, canvas.height],
-    },
-    target: 'screen',
-  })
+    tex: passes.gi.b,
+    u_resolution: [canvas.width, canvas.height]
+  }, 'screen')
 
   tick++
   console.log(tick)
-  if (tick < 3)
+  if (tick < 3) {
     requestAnimationFrame(draw)
+  }
 }
 
 draw()
@@ -170,9 +136,9 @@ window.addEventListener('resize', (e) => {
   resize()
 })
 
-function resize() {
-  let w = window.innerWidth * window.devicePixelRatio
-  let h = window.innerHeight * window.devicePixelRatio
+function resize () {
+  const w = window.innerWidth * window.devicePixelRatio
+  const h = window.innerHeight * window.devicePixelRatio
   twgl.resizeFramebufferInfo(gl, passes.gi.buffer, passes.gi.attachments, w, h)
   twgl.resizeFramebufferInfo(gl, passes.gi.backbuffer, passes.gi.attachments, w, h)
   passes.gi.resolution = [w, h]
---
+++ voxels-gi/readme.md
@@ -0,0 +1,18 @@
+# Voxels-GI
+
+This folder is a VS Code project.
+
+## Building
+
+Only the first time:
+
+1. `npm ci`
+2. The same for inner project:
+   1. `cd z-shader-wrapper-2`
+   2. `npm ci`
+
+Every time: _Tasks: Run Build Task_
+
+## Running
+
+The same as building.
